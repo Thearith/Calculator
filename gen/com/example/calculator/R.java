a73d48da472decafa0663b396506b44a847c81df
Thearith <sothearith.sreang@gmail.com>
2013-12-13 12:49:55 +0700

    add two more buttons

--- gen/com/example/calculator/R.java
+++ gen/com/example/calculator/R.java
@@ -24,21 +24,23 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000f;
-        public static final int buttonCE=0x7f08000e;
+        public static final int action_settings=0x7f080011;
+        public static final int buttonCE=0x7f08000d;
         public static final int buttonDivide=0x7f080004;
         public static final int buttonEight=0x7f080002;
+        public static final int buttonEqual=0x7f08000f;
         public static final int buttonFive=0x7f080006;
         public static final int buttonFour=0x7f080005;
         public static final int buttonMul=0x7f080008;
         public static final int buttonNine=0x7f080003;
         public static final int buttonOne=0x7f080009;
+        public static final int buttonPlus=0x7f080010;
         public static final int buttonSeven=0x7f080001;
         public static final int buttonSix=0x7f080007;
         public static final int buttonSub=0x7f08000c;
         public static final int buttonThree=0x7f08000b;
         public static final int buttonTwo=0x7f08000a;
-        public static final int buttonZero=0x7f08000d;
+        public static final int buttonZero=0x7f08000e;
         public static final int displayBox=0x7f080000;
     }
     public static final class layout {
@@ -51,7 +53,7 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
-        public static final int invisibleText=0x7f050012;
+        public static final int invisibleText=0x7f050013;
         public static final int number_0=0x7f05000c;
         public static final int number_1=0x7f050003;
         public static final int number_2=0x7f050004;
@@ -64,6 +66,7 @@ public final class R {
         public static final int number_9=0x7f05000b;
         public static final int operator_CE=0x7f050011;
         public static final int operator_divide=0x7f050010;
+        public static final int operator_equal=0x7f050012;
         public static final int operator_mul=0x7f05000f;
         public static final int operator_plus=0x7f05000d;
         public static final int operator_sub=0x7f05000e;
